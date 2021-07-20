1ea6c2d8e3e988a95e93fdd306077a60421817ae
yeonsuan <sweetsuan@naver.com>
2021-07-20 13:57:43 +0900

    react style

---
+++ suan0720/week-1/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const styles ={
+    border : '1px solid #eee',
+    padding : '20px',
+    display : 'flex',
+    width : '100vw',
+    maxWidth : '400px',
+    margin : '30px auto',
+    flexDirection : 'column',
+  };
+
+  return (
+    <div className="App">
+     <div style={styles}>
+       <h1 style={{color:'green'}}>안녕하세요!</h1>
+       <hr style={{width:'100%'}}/>
+       <p style={{textAlign:'left'}}>이름을 입력하세요</p>
+       <input type="text"/>
+     </div>
+    </div>
+  );
+}
+
+export default App;
